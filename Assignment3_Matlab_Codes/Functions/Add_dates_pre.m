f29cb9821238a1f5f8b26f8ee5cd8f429dfc54e4
Anshk16 <85384439+Anshk16@users.noreply.github.com>
2025-04-04 13:20:21 +0530

    Add files via upload
    Assignment 3(Matlab)

---
+++ Assignment3_Matlab_Codes/Functions/Add_dates_pre.m
@@ -0,0 +1,21 @@
+function datesSet_add = Add_dates_pre(dt, n)
+    % Converte dt in datetime se non lo è già
+    if ~isa(dt, 'datetime')
+        dt = datetime(dt, 'ConvertFrom', 'datenum');
+    end
+
+    % Inizializza un array di NaT per memorizzare le date
+    datesSet_add = NaT(n+1, 1);
+
+    % Genera la sequenza di date
+    for i = 0:n
+        datesSet_add(i+1) = dt + calyears(i); % Aggiunge i anni alla data iniziale
+        
+        % Controlla se la data cade nel weekend e la sposta al lunedì successivo
+        if weekday(datesSet_add(i+1)) == 7
+            datesSet_add(i+1) = datesSet_add(i+1) - days(1);  % Sabato → Lunedì
+        elseif weekday(datesSet_add(i+1)) == 1
+            datesSet_add(i+1) = datesSet_add(i+1) - days(2);  % Domenica → Lunedì
+        end
+    end
+end
